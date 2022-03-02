e3a9f302fac4003f052eb9cd050d11da94b43de3
Lukas Vranic <Lukasvranic@gmail.com>
2022-03-02 17:13:25 +0100

    did stuff

--- sketch.js
+++ sketch.js
@@ -7,15 +7,13 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
   div = new Cdiv(100, 100, 200, 200);
-  frameRate(120);
 }
 
 function draw() {
   resizeCanvas(windowWidth, windowHeight);
-  background(220);
-  background(220);
+  background(200);
   div.update();
-  div.C_input();
+  div.C_input(50, 50);
   div.over();
 }
 
@@ -36,6 +34,8 @@ class Cdiv {
     this.offsetX = 0;
     this.offsetY = 0;
     this.inp = createInput("");
+    this.btn = createButton("Add");
+    this.close = false;
   }
 
   over() {
@@ -71,22 +71,49 @@ class Cdiv {
     this.dragging = false;
   }
 
-  C_input() {
-    this.inp.position(this.x + 5, this.y + 5);
+  C_input(x, y) {
+    this.inp.position(x + 5, y + 5);
     this.inp.style("height", `${20}px`);
-    this.inp.style("width", `${this.w - 20}px`);
+    this.inp.style("width", `${this.w - 70}px`);
+    this.btn.position(x + this.w - 50, y + 5);
+    this.btn.style("height", `${25}px`);
+    this.btn.style("width", `${40}px`);
   }
+
+  header() {
+    push();
+    fill(220);
+    if (this.rollover) {
+      fill(120);
+    }
+    rect(this.x, this.y - 20, this.w, 20);
+    if (this.rollover) {
+      fill(255);
+    } else {
+      fill(0);
+    }
+    textSize(20);
+    text("TO DO", this.x + 10, this.y - 5 / 2);
+    if (
+      mouseX > this.x + this.w - 20 &&
+      mouseX < this.x + this.w - 5 &&
+      mouseY > this.y - 20 &&
+      mouseY < this.y
+    ) {
+      this.close = true;
+      stroke(255, 0, 0);
+    }
+    strokeWeight(2);
+    line(this.x + this.w - 20, this.y - 10, this.x + this.w - 5, this.y - 10);
+    pop();
+  }
+
   update() {
     if (this.dragging) {
       this.x = mouseX + this.offsetX;
       this.y = mouseY + this.offsetY;
     }
-    push();
-    if (this.rollover) {
-      fill(120);
-    }
-    rect(this.x, this.y - 20, this.w, 20);
-    pop();
+    this.header();
     rect(this.x, this.y, this.w, this.h);
   }
 }
