c83b7a7f5bf0a3b02105fb5a487fd5916f9a6f03
Lukas Vranic <Lukasvranic@gmail.com>
2022-03-21 15:10:29 +0100

    added header

--- Start-page-2/src/script.js
+++ Start-page-2/src/script.js
@@ -1,3 +1,4 @@
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
 class box {
   constructor(name, x, y, w, h) {
     this.name = name;
@@ -10,17 +11,32 @@ class box {
 
   show() {
     this.box.style.position = "fixed ";
-    this.box.style.top = `${this.x}px`;
-    this.box.style.left = `${this.y}px`;
+    this.box.style.top = `${this.y}px`;
+    this.box.style.left = `${this.x}px`;
     this.box.style.width = `${this.w}px`;
     this.box.style.height = `${this.h}px`;
     this.box.style.backgroundColor = "rgb(220, 220, 220)";
     this.box.style.border = "2px solid black";
     this.box.style.cursor = "pointer";
     this.box.setAttribute("onclick", `${this.name}.onclick()`);
-    this.box.setAttribute("onmouseover", `${this.name}.onmouseover()`);
-    this.box.setAttribute("onmouseout", `${this.name}.onmouseout()`);
     document.body.appendChild(this.box);
+  }
+
+  header(text) {
+    this.header = document.createElement("div");
+    this.header.style.position = "fixed ";
+    this.header.innerHTML += text;
+    this.header.style.top = `${this.y - 20}px`;
+    this.header.style.left = `${this.x}px`;
+    this.header.style.width = `${this.w}px`;
+    this.header.style.height = `${20}px`;
+    this.header.style.backgroundColor = "rgb(220, 220, 220)";
+    this.header.style.border = "2px solid black";
+    this.header.setAttribute("onmouseover", `${this.name}.onmouseover()`);
+    this.header.setAttribute("onmouseout", `${this.name}.onmouseout()`);
+    document.body.appendChild(this.header);
+
+    console.log(this.x);
   }
 
   onclick() {
@@ -28,13 +44,14 @@ class box {
   }
 
   onmouseover() {
-    this.box.style.backgroundColor = "rgb(240, 240, 240)";
+    this.header.style.backgroundColor = "rgb(240, 240, 240)";
   }
 
   onmouseout() {
-    this.box.style.backgroundColor = "rgb(220, 220, 220)";
+    this.header.style.backgroundColor = "rgb(220, 220, 220)";
   }
 }
 
 test = new box("test", 100, 200, 100, 100);
+test.header("TEST");
 test.show();
