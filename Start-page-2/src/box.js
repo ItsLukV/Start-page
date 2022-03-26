7934dae909605dc82102a6c3cdb5fd9bf225bde3
Lukas <lukasvranic@gmail.com>
2022-03-26 14:21:38 +0100

    added list

--- Start-page-2/src/box.js
+++ Start-page-2/src/box.js
@@ -1,16 +1,15 @@
+let list = [];
+
 $(function () {
   $(".drag").draggable({
     cursor: "crosshair",
-    drag: function () {
-      // $("#box").offset() = $("#header").offset()
-    },
     stop: function () {
       console.log($(".box").offset());
     },
   });
 
   $("#box").resizable({
-    handles: "w,e,s",
+    handles: "e,s",
     minWidth: 200,
     maxWidth: 400,
     minHeight: 200,
@@ -18,6 +17,57 @@ $(function () {
     ghost: true,
     stop: function () {
       $("#header").width($("#box").width());
+      checkHide();
     },
+    resize: function () {},
   });
+
+  $("#btnSubmit").click(function () {
+    create($("#input").val());
+  });
+
+  function checkHide() {
+    if (
+      $("#btnSubmit").position().top + $("#btnSubmit").height() + 5 >
+      $("#box").height() + $(".box").position().top
+    ) {
+      $("#btnSubmit").hide();
+      $("#input").hide();
+    } else {
+      $("#btnSubmit").show();
+      $("#input").show();
+    }
+    for (let i = 0; i < list.length; i++) {
+      if (
+        $(`#li${i}`).position().top + $(`#li${i}`).height() + 5 >
+        $("#box").height() + $(".box").position().top
+      ) {
+        $(`#li${i}`).hide();
+      } else {
+        $(`#li${i}`).show();
+      }
+    }
+  }
+
+  function create(text) {
+    list.push(
+      $("ol").append(
+        `<li id="li${list.length}">${text}<button onclick="remove(${list.length})" id="btn${list.length}" class="close">x</button></li>`
+      )
+    );
+    checkHide();
+    // $(".box").position().top;
+    // $(".box").height();
+  }
 });
+
+function arrRemove(arr, x) {
+  if (x === undefined) return;
+  arr.splice(x, 1);
+}
+
+function remove(x) {
+  $("#li" + x).remove();
+  arrRemove(list, x);
+  checkHide();
+}
